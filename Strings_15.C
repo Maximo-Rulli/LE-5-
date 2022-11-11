ee2d33038cacc352458e92a93a0fa58a07f21556
Maximo-Rulli <75349174+Maximo-Rulli@users.noreply.github.com>
2022-11-11 11:50:03 -0300

    Update Strings_15.C

--- Strings_15.C
+++ Strings_15.C
@@ -1,23 +1,33 @@
 #include <stdio.h>
+#include <stdio_ext.h>
 
-int contador_vocal(char *);
+int contador_vocal(char []);
 int main(void) {
     unsigned int seguir = 1;
-    unsigned char mi_string[20];
+    unsigned char mi_string[21];
     while(seguir!=0){
         printf("Ingrese una palabra de longitud 20 como máximo: ");
+        /*
+        Con esto la consigna es cumplida pero para mayor comodidad del usuario se usa solo un scanf() con %s
+        for(unsigned char i = 0; i<21; i++){
+            scanf("%c", &mi_string[i]);
+            __fpurge(stdin);
+        }
+        */
         scanf("%s", mi_string);
+        __fpurge(stdin);
         contador_vocal(mi_string);
         printf("\nPara cerrar el programa presione 0: ");
         scanf("%u", &seguir);
+        __fpurge(stdin);
     }
     return 0;
 }
 
-int contador_vocal(char * mi_string){
+int contador_vocal(char mi_string[]){
     int vocales[] =          {0,   0,   0,   0,   0};
     char vocales_letras[] = {'a', 'e', 'i', 'o', 'u'};
-    for(int i = 0; i<20; i++){
+    for(int i = 0; i<21; i++){
         if(mi_string[i] == 'a'){
             vocales[0]++;
     }

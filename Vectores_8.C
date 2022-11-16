4adb99dcf2d01781b85675a12da7bf632236af52
Maximo-Rulli <75349174+Maximo-Rulli@users.noreply.github.com>
2022-11-16 10:08:13 -0300

    Update Vectores_8.C

--- Vectores_8.C
+++ Vectores_8.C
@@ -2,6 +2,7 @@
 #include <stdio_ext.h>
 
 void buscar_num(int longitud);
+
 int main(void) {
     unsigned int seguir = 1;
     unsigned int longitud;
@@ -9,7 +10,7 @@ int main(void) {
         printf("Ingrese la cantidad de numeros que ingresara: ");
         scanf("%d", &longitud);
         __fpurge(stdin);
-        buscar_letra(longitud);
+        buscar_num(longitud);
         printf("\nPara cerrar el programa presione 0: ");
         scanf("%d", &seguir);
         __fpurge(stdin);
@@ -20,6 +21,7 @@ int main(void) {
 void buscar_num(int longitud){
     int numeros[longitud];
     int numero_buscar = 0;
+    char encontrar = 0;
     for(unsigned int i = 0; i<longitud; i++){
         printf("\nIngrese el %d° numero: ", ++i);
         scanf("%d", &numeros[--i]);
@@ -30,10 +32,10 @@ void buscar_num(int longitud){
     __fpurge(stdin);
     for(unsigned int i = 0; i<longitud; i++){
         if(numeros[i] == numero_buscar){
-            printf("\nEl numero %d se encuentra por primera vez en la posición %d", numero_buscar, i);
-            break;
+            printf("\nEl numero %d se encuentra en la posición %d", numero_buscar, i);
+            encontrar = 1;
         }
-        if((numeros[(longitud-1)] != numero_buscar) && (i == longitud-1)){
+        if((numeros[(longitud-1)] != numero_buscar) && (i == longitud-1) && (encontrar==0)){
             printf("No se encontro %d entre los números ingresados", numero_buscar);
             printf("\n-1");
         }
